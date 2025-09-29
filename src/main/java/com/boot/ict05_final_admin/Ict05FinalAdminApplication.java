950ae1d8943b16ed3083ffea87773b6e8e12a287
DONG-GWAN KIM <gek2omg@gmail.com>
2025-09-29 13:10:14 +0900

    init

---
+++ src/main/java/com/boot/ict05_final_admin/Ict05FinalAdminApplication.java
@@ -0,0 +1,13 @@
+package com.boot.ict05_final_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ict05FinalAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ict05FinalAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boot/ict05_final_admin/Ict05FinalAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.ict05_final_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ict05FinalAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
