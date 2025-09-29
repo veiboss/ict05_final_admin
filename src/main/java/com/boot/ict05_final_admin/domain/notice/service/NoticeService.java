5a66b527f8391112250548a34bdda22f8d731722
Park miri <already0310@gmail.com>
2025-09-29 17:41:02 +0900

    빈 메서드 insertOfficeNotice

--- src/main/java/com/boot/ict05_final_admin/domain/notice/service/NoticeService.java
+++ src/main/java/com/boot/ict05_final_admin/domain/notice/service/NoticeService.java
@@ -1,9 +1,24 @@
 package com.boot.ict05_final_admin.domain.notice.service;
 
+import com.boot.ict05_final_admin.domain.notice.dto.NoticeDetailDTO;
+import com.boot.ict05_final_admin.domain.notice.dto.NoticeWriteFormDTO;
+import com.boot.ict05_final_admin.domain.notice.repository.NoticeRepositoryImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+
+@RequiredArgsConstructor
 @Service
 public class NoticeService {
 
-    public
+    private final NoticeRepositoryImpl noticeRepository;
+
+    public NoticeWriteFormDTO insertOfficeNotice(NoticeWriteFormDTO noticeDetailDTO) {
+        System.out.println("NoticeService - insertOfficeNotice");
+
+        noticeRepository
+
+
+        return null;
+    }
 }
