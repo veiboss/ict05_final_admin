4034f83a659bdf0ec7d1bc0a1e90f6af67963e3d
Kyeongwook <lgu823@naver.com>
2025-09-29 17:35:00 +0900

    수정

--- src/main/java/com/boot/ict05_final_admin/domain/notice/entity/NoticePriority.java
+++ src/main/java/com/boot/ict05_final_admin/domain/notice/entity/NoticePriority.java
@@ -1,5 +1,17 @@
 package com.boot.ict05_final_admin.domain.notice.entity;
 
 public enum NoticePriority {
-    NOMAL, IMPORTANT, EMERGENCY
+    NOMAL("일반"),
+    IMPORTANT("중요"),
+    EMERGENCY("긴급");
+
+    private final String description;
+
+    NoticePriority(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
