f3204f729b7a32b6f0bd34387d66be9b2650e471
f_minjin <cmjmj5656@gmail.com>
2025-09-29 18:21:19 +0900

    swagger추가중

---
+++ src/main/java/com/boot/ict05_final_admin/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.boot.ict05_final_admin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests(auth -> auth
+                        // Swagger 관련 경로 허용
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs.yaml"
+                        ).permitAll()
+                        // 나머지는 인증 필요
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
+
+
+
--- src/main/java/com/boot/ict05_final_admin/domain/notice/controller/NoticeRestController.java
+++ src/main/java/com/boot/ict05_final_admin/domain/notice/controller/NoticeRestController.java
@@ -1,5 +1,8 @@
 package com.boot.ict05_final_admin.domain.notice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import com.boot.ict05_final_admin.domain.notice.dto.NoticeWriteFormDTO;
 import com.boot.ict05_final_admin.domain.notice.entity.Notice;
 import com.boot.ict05_final_admin.domain.notice.service.NoticeService;
@@ -14,13 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/admin")
+@Tag(name = "공지사항 API", description = "공지사항 등록/조회/수정/삭제 기능 제공")
 public class NoticeRestController {
 
     private final NoticeService noticeService;
 
     @PostMapping("/notice/write")
-    public ResponseEntity<Long> addOfficeNotice(@RequestBody NoticeWriteFormDTO dto) {
-        Long id = noticeService.insertOfficeNotice(dto);
-        return ResponseEntity.status(HttpStatus.OK).body(id);
+    @Operation(summary = "공지사항 등록", description = "본사에서 새로운 공지사항을 등록하는 API")
+    public ResponseEntity<Notice> addOfficeNotice(@RequestBody NoticeWriteFormDTO dto) {
+        Notice notice = noticeService.insertOfficeNotice(dto);
+        return ResponseEntity.ok().body(notice);
     }
 }
