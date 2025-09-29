b625d2523e4ea3711d1241ebece60856002af063
DONG-GWAN KIM <gek2omg@gmail.com>
2025-09-29 15:43:28 +0900

    Notice add

--- src/main/java/com/boot/ict05_final_admin/domain/notice/repository/NoticeRepositoryImpl.java
+++ src/main/java/com/boot/ict05_final_admin/domain/notice/repository/NoticeRepositoryImpl.java
@@ -1,7 +1,12 @@
 package com.boot.ict05_final_admin.domain.notice.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class NoticeRepositoryImpl implements NoticeRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
 }
