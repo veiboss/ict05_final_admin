bf26975703535f307e01a937e2eff9e0ccfb7b59
Kyeongwook <lgu823@naver.com>
2025-09-29 17:57:45 +0900

    레스트 컨트롤러 수정

--- src/main/java/com/boot/ict05_final_admin/domain/notice/controller/NoticeRestController.java
+++ src/main/java/com/boot/ict05_final_admin/domain/notice/controller/NoticeRestController.java
@@ -4,6 +4,7 @@ import com.boot.ict05_final_admin.domain.notice.dto.NoticeWriteFormDTO;
 import com.boot.ict05_final_admin.domain.notice.entity.Notice;
 import com.boot.ict05_final_admin.domain.notice.service.NoticeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,8 +19,8 @@ public class NoticeRestController {
     private final NoticeService noticeService;
 
     @PostMapping("/notice/write")
-    public ResponseEntity<Notice> addOfficeNotice(@RequestBody NoticeWriteFormDTO dto) {
-        Notice notice = noticeService.insertOfficeNotice(dto);
-        return ResponseEntity.ok().body(notice);
+    public ResponseEntity<Long> addOfficeNotice(@RequestBody NoticeWriteFormDTO dto) {
+        Long id = noticeService.insertOfficeNotice(dto);
+        return ResponseEntity.status(HttpStatus.OK).body(id);
     }
 }
