de7e0bcca73bceb99d79968454b51aa2d30c57d7
DONG-GWAN KIM <gek2omg@gmail.com>
2025-09-29 16:50:56 +0900

    NoticeCategory

--- src/main/java/com/boot/ict05_final_admin/domain/notice/entity/NoticeCategory.java
+++ src/main/java/com/boot/ict05_final_admin/domain/notice/entity/NoticeCategory.java
@@ -1,5 +1,18 @@
 package com.boot.ict05_final_admin.domain.notice.entity;
 
 public enum NoticeCategory {
-    NOMAL, SYSTEM, EVENT, POLICY
+    NOMAL("일반"),
+    SYSTEM("시스템"),
+    EVENT("이벤트"),
+    POLICY("공지");
+
+    private final String description;
+
+    NoticeCategory(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
