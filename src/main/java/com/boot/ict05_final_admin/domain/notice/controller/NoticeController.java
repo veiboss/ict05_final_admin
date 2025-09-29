163c7b330df1a582289dfb60e0f4ef1f92d2a087
Kyeongwook <lgu823@naver.com>
2025-09-29 16:31:43 +0900

    오류 수정

--- src/main/java/com/boot/ict05_final_admin/domain/notice/controller/NoticeController.java
+++ src/main/java/com/boot/ict05_final_admin/domain/notice/controller/NoticeController.java
@@ -1,7 +1,15 @@
 package com.boot.ict05_final_admin.domain.notice.controller;
 
+import com.boot.ict05_final_admin.domain.notice.service.NoticeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class NoticeController {
+
+     private final NoticeService noticeService;
+
+     //@GetMapping("notice/write")
 }
