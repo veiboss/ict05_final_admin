3e76a9ba07d868042c2651b881300774a122aea4
f_minjin <cmjmj5656@gmail.com>
2025-10-13 16:18:09 +0900

    발주_파일생성

---
+++ src/main/java/com/boot/ict05_final_admin/domain/purchase_order/controller/PurchaseOrderController.java
@@ -0,0 +1,4 @@
+package com.boot.ict05_final_admin.domain.purchase_order.controller;
+
+public class PurchaseOrderController {
+}
--- src/test/java/com/boot/ict05_final_admin/domain/notice/service/NoticeServiceTest.java
+++ src/test/java/com/boot/ict05_final_admin/domain/notice/service/NoticeServiceTest.java
@@ -14,21 +14,21 @@ class NoticeServiceTest {
     @Autowired
     private NoticeService noticeService;
 
-    @Test
-    void insertThousandNotices() {
-        for (int i = 1; i <= 1000; i++) {
-            NoticeWriteFormDTO dto = new NoticeWriteFormDTO();
-            dto.setNoticeCategory(NoticeCategory.NORMAL);   // 카테고리 선택
-            dto.setNoticePriority(NoticePriority.NORMAL);   // 우선순위 선택
-            dto.setTitle("테스트 게시물 제목 " + i);
-            dto.setBody("<p>테스트 게시물 내용 " + i + "</p>");
-            dto.setWriter("테스트유저");
-
-            noticeService.insertOfficeNotice(dto);
-
-            if (i % 100 == 0) {
-                System.out.println(i + "개 게시물 저장 완료");
-            }
-        }
-    }
+//    @Test
+//    void insertThousandNotices() {
+//        for (int i = 1; i <= 1000; i++) {
+//            NoticeWriteFormDTO dto = new NoticeWriteFormDTO();
+//            dto.setNoticeCategory(NoticeCategory.NORMAL);   // 카테고리 선택
+//            dto.setNoticePriority(NoticePriority.NORMAL);   // 우선순위 선택
+//            dto.setTitle("테스트 게시물 제목 " + i);
+//            dto.setBody("<p>테스트 게시물 내용 " + i + "</p>");
+//            dto.setWriter("테스트유저");
+//
+//            noticeService.insertOfficeNotice(dto);
+//
+//            if (i % 100 == 0) {
+//                System.out.println(i + "개 게시물 저장 완료");
+//            }
+//        }
+//    }
 }
