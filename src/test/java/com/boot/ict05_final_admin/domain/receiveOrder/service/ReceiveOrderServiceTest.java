8474c999543dc74dc2ddfd5f9934382230f17447
f_minjin <cmjmj5656@gmail.com>
2025-10-15 18:27:50 +0900

    수주_목록_1차완

---
+++ src/test/java/com/boot/ict05_final_admin/domain/receiveOrder/service/ReceiveOrderServiceTest.java
@@ -0,0 +1,36 @@
+package com.boot.ict05_final_admin.domain.receiveOrder.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReceiveOrderServiceTest {
+
+    @Test
+    void selectAllOfficeReceive() {
+//        for (int i = 1; i = <= 1000; i++) {
+//
+//        }
+
+
+
+
+//        for (int i = 1; i <= 1000; i++) {
+//            NoticeWriteFormDTO dto = new NoticeWriteFormDTO();
+//            dto.setNoticeCategory(NoticeCategory.NORMAL);   // 카테고리 선택
+//            dto.setNoticePriority(NoticePriority.NORMAL);   // 우선순위 선택
+//            dto.setTitle("테스트 게시물 제목 " + i);
+//            dto.setBody("<p>테스트 게시물 내용 " + i + "</p>");
+//            dto.setWriter("테스트유저");
+//
+//            noticeService.insertOfficeNotice(dto);
+//
+//            if (i % 100 == 0) {
+//                System.out.println(i + "개 게시물 저장 완료");
+//            }
+    }
+
+    @Test
+    void findById() {
+    }
+}
