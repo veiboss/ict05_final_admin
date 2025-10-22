554e5139ef3b9de2052dabd4a39c6e45d23848e4
ok chaeeun <cosmoscenery7@gmail.com>
2025-10-22 19:00:30 +0900

    알러지 삭제

--- src/main/java/com/boot/ict05_final_admin/domain/inventory/entity/Material.java
+++ src/main/java/com/boot/ict05_final_admin/domain/inventory/entity/Material.java
@@ -1,6 +1,5 @@
 package com.boot.ict05_final_admin.domain.inventory.entity;
 
-import com.boot.ict05_final_admin.domain.menu.entity.Allergy;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -98,9 +97,5 @@ public class Material {
     @Column(name = "material_modify_date", nullable = false,
             columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일'")
     private LocalDateTime modifyDate;
-
-    /** 재료 알레르기 관계 */
-    @ManyToMany
-    @Builder.Default
-    private Set<Allergy> allergies = new LinkedHashSet<>();
+    
 }
--- src/main/java/com/boot/ict05_final_admin/domain/menu/repository/AllergyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.boot.ict05_final_admin.domain.menu.repository;
-
-import com.boot.ict05_final_admin.domain.menu.entity.Allergy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AllergyRepository  extends JpaRepository<Allergy, Integer> { }
-
-
